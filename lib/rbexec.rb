72140130027c67c3a4d98ae0b66ad8063370903a
John Hawthorn <john@hawthorn.email>
2024-04-21 20:55:43 -0700

    Initial commit

---
+++ lib/rbexec.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "rbexec/version"
+
+module Rbexec
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/rbexec/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module Rbexec
+  VERSION = "0.1.0"
+end
